1ad3904d2f116ca03b3b78c785dfb052ab3e3df3
chunlan <2680064189@qq.com>
2015-12-22 22:30:50 +0800

    add annotate

--- 1.1.5.各种outofmemory/heap.java
+++ 1.1.5.各种outofmemory/heap.java
@@ -2,6 +2,8 @@
  * 堆溢出
  * 定义一个动态数组，往里面添加对象，使它占据25M的大小，在命令行下执行
  * java -Xmx20m -Xms5m heap
+ * 我们还可以添加参数-XX:+HeapDumpOnOutOfMemoryError,在内存溢出时导出整个堆信息
+ * 配合上面参数使用有-XX:HeapDumpPath=d:/a.dump,来指定导出堆的位置
  */
 
 import java.util.Vector;
